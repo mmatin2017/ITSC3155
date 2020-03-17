c6806ac9036661a611cf1718180d62dd41811685
Mohammed <mmatin@uncc.edu>
2020-03-17 18:26:51 +0000

    Completed Create and show

--- blog/app/controllers/articles_controller.rb
+++ blog/app/controllers/articles_controller.rb
@@ -2,6 +2,15 @@ class ArticlesController < ApplicationController
     def new
     end
     def create
-        render plain: params[:article].inspect
+        @article = Article.new(articles_params)
+        
+        @article.save
+        redirect_to @article
     end
+    private def articles_params
+    params.require(:article).permit(:title, :text)
+end
+def show
+    @article = Article.find(params[:id])
+end
 end
