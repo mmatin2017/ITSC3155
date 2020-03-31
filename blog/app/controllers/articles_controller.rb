6d2c5117f72e6f49435c3b8369a4b9b290100d81
Mohammed <mmatin@uncc.edu>
2020-03-31 19:33:05 +0000

    Add edit/update method

--- blog/app/controllers/articles_controller.rb
+++ blog/app/controllers/articles_controller.rb
@@ -21,4 +21,16 @@ end
 def index
     @articles = Article.all
 end
+def edit
+    @article = Article.find(params[:id])
+end 
+def update
+        @article = Article.find(params[:id])
+    if @article.update(articles_params)
+        redirect_to @article
+    else
+        render 'edit'
+    end
+end
+
 end
